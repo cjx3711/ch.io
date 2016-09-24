9b43d42b47e7feea20cbefecd74d5afcf8fe8bc3
Jia Xun <jiaxun.chai@scriptrock.com>
2016-09-23 22:57:37 -0700

    Bootstrap and angular skeleton working.

---
+++ ch.io/public/angular/app.js
@@ -0,0 +1,11 @@
+app.config( function($routeProvider) {
+  $routeProvider.when("/", {
+      templateUrl : "templates/home.html",
+      controller: "homeController"
+  })
+});
+
+
+app.controller('homeController', function ($scope) {
+
+});

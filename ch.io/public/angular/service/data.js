786f30cb3ee4db874ea8f32c281d1702f3e10e7a
Jia Xun <chaijiaxun@gmail.com>
2016-09-24 15:47:08 -0700

    Skeleton for merv

--- ch.io/public/angular/service/data.js
+++ ch.io/public/angular/service/data.js
@@ -3,4 +3,16 @@ app.service('dataService', function() {
   this.sendRawTextInput = function(data) {
     console.log("Sending data", data);
   }
+
+
+  //TODO: MERV HERE
+
+  this.attractions = [];
+  this.attractions.push({
+    id: 0,
+    name: "Hello",
+    pos: { x: 100, y: 100 },
+    category: "Thing",
+    tags: ["tag1", "tag2"]
+  });
 });

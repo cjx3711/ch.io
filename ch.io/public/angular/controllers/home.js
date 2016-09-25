bd3a2f2cc22e9ff292c2dc9ed63210c5f649c538
Haritha <haritha1211@gmail.com>
2016-09-25 12:37:03 -0700

    blah

--- ch.io/public/angular/controllers/home.js
+++ ch.io/public/angular/controllers/home.js
@@ -1,9 +1,7 @@
 app.controller('homeController', ['$scope', 'dataService', function ($scope, dataService) {
   $scope.message = dataService.welcomeMessage;
-  $scope.something = "hello;";
   $scope.submitData = function() {
     dataService.sendRawTextInput($scope.customerInput);
   }
-
 
 }]);

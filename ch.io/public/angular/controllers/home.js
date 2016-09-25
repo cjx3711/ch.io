93ad4da4a2261d6aa3d33fc381cbe471164942af
Yao Xiang <yaoxiang@Yaos-MacBook-Pro.local>
2016-09-24 17:48:13 -0700

    Add next and back button.

--- ch.io/public/angular/controllers/home.js
+++ ch.io/public/angular/controllers/home.js
@@ -3,7 +3,11 @@ app.controller('homeController', ['$scope', 'dataService', function ($scope, dat
   $scope.submitData = function() {
     dataService.sendRawTextInput($scope.customerInput);
   }
-  $scope.data = { "question": "This is q1",
+  $scope.data = [{"question": "This is q1",
                   "options": ["Choice 1", "Choice 2"]
-                };
+                },{"question": "This is q2",
+                  "options": ["Choice 3", "Choice 4"]
+                },{"question": "This is q3",
+                  "options": ["Choice 5", "Choice 6"]
+                }];
 }]);
--- ch.io/public/angular/controllers/survey.js
+++ ch.io/public/angular/controllers/survey.js
@@ -1,6 +1,31 @@
 app.controller('surveyController', ['$scope', 'dataService', function ($scope, dataService) {
 	//This is the data sent in from the survey directive
-	$scope.question = $scope.data.question;
-	$scope.options = $scope.data.options; //Array
+	$scope.answers = [];
+	$scope.questionObj = [];
+	for (var i = 0, len = $scope.data.length; i < len; i++) {
+       $scope.questionObj.push($scope.data[i]);
+    }
 
+    $scope.currentQuestion = 0;
+    $scope.isLastQuestion = (($scope.questionObj.length - 1) <= $scope.currentQuestion);
+    $scope.isFirstQuestion = true;
+
+	$scope.question = $scope.questionObj[$scope.currentQuestion].question;
+	$scope.options = $scope.questionObj[$scope.currentQuestion].options;
+    
+    $scope.nextQuestion = function() {
+    	$scope.currentQuestion += 1;
+        $scope.isFirstQuestion = ($scope.currentQuestion <= 0);
+        $scope.isLastQuestion = (($scope.questionObj.length - 1) <= $scope.currentQuestion);
+        $scope.question = $scope.questionObj[$scope.currentQuestion].question;
+        $scope.options = $scope.questionObj[$scope.currentQuestion].options;
+    }
+    $scope.prevQuestion = function() {
+    	$scope.currentQuestion -= 1;
+        $scope.isFirstQuestion = ($scope.currentQuestion <= 0);
+        $scope.isLastQuestion = (($scope.questionObj.length - 1) <= $scope.currentQuestion);
+        console.log($scope.isFirstQuestion);
+        $scope.question = $scope.questionObj[$scope.currentQuestion].question;
+        $scope.options = $scope.questionObj[$scope.currentQuestion].options;
+    }
 }]);

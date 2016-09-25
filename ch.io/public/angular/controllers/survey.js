b9e68376e0d97f325d7f4723b768f71b54dd1ca5
Yao Xiang <yaoxiang@Yaos-MacBook-Pro.local>
2016-09-24 18:00:43 -0700

    Remembers choices of user in array

--- ch.io/public/angular/controllers/survey.js
+++ ch.io/public/angular/controllers/survey.js
@@ -1,6 +1,6 @@
 app.controller('surveyController', ['$scope', 'dataService', function ($scope, dataService) {
 	//This is the data sent in from the survey directive
-	$scope.answers = [];
+	$scope.answers = Array();
 	$scope.questionObj = [];
 	for (var i = 0, len = $scope.data.length; i < len; i++) {
        $scope.questionObj.push($scope.data[i]);
@@ -13,13 +13,26 @@ app.controller('surveyController', ['$scope', 'dataService', function ($scope, d
 	$scope.question = $scope.questionObj[$scope.currentQuestion].question;
 	$scope.options = $scope.questionObj[$scope.currentQuestion].options;
     
-    $scope.nextQuestion = function() {
+    $scope.nextQuestion = function(ans) {
     	$scope.currentQuestion += 1;
         $scope.isFirstQuestion = ($scope.currentQuestion <= 0);
-        $scope.isLastQuestion = (($scope.questionObj.length - 1) <= $scope.currentQuestion);
-        $scope.question = $scope.questionObj[$scope.currentQuestion].question;
-        $scope.options = $scope.questionObj[$scope.currentQuestion].options;
+        $scope.isLastQuestion = (($scope.questionObj.length - 1) < $scope.currentQuestion);
+        if (ans) {
+	        $scope.answers.push(ans);
+        } else {
+        	$scope.answers.push("");
+        }
+        if (($scope.questionObj.length - 1) < $scope.currentQuestion) {
+ 			//Finish survey.. continue to post processing
+ 			$scope.question = "Done!";
+ 			$scope.options = [];
+ 			console.log($scope.answers);
+        } else {
+    	    $scope.question = $scope.questionObj[$scope.currentQuestion].question;
+        	$scope.options = $scope.questionObj[$scope.currentQuestion].options;
+        }
     }
+/*  --Unused Prev Question 
     $scope.prevQuestion = function() {
     	$scope.currentQuestion -= 1;
         $scope.isFirstQuestion = ($scope.currentQuestion <= 0);
@@ -28,4 +41,5 @@ app.controller('surveyController', ['$scope', 'dataService', function ($scope, d
         $scope.question = $scope.questionObj[$scope.currentQuestion].question;
         $scope.options = $scope.questionObj[$scope.currentQuestion].options;
     }
+*/
 }]);

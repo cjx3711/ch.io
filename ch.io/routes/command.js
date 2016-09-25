80a82dcac7f6c3337975c7b05aafeac1f02288d7
Jia Xun <chaijiaxun@gmail.com>
2016-09-24 17:25:35 -0700

    Parser skeleton

--- ch.io/routes/command.js
+++ ch.io/routes/command.js
@@ -13,7 +13,37 @@
  });
  router.post('/', function(req, res, next) {
    // res.send('respond with a resource');
+   var rawString = req.body.data;
+
+
    res.json({body: req.body.data });
  });
+
+
+var ITINERARY = 'itinerary';
+var LOCATION = 'location';
+var PURCHASE = 'purchase';
+
+// Input cases:
+//   What can I do in 5 hours?
+//   I need to buy a gift
+
+function parseCategory(rawString) {
+
+}
+function getData(category, rawString) {
+  switch (category) {
+    case ITINERARY:
+      // Parse the data
+    break;
+    case LOCATION:
+
+    break;
+
+    case PURCHASE:
+
+    break;
+  }
+}
 
  module.exports = router;

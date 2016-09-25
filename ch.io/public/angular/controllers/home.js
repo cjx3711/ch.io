284b8d8d8fc4efe36cec65fbb48be7db66699c15
Yao Xiang <yaoxiang@Yaos-MacBook-Pro.local>
2016-09-24 18:50:16 -0700

    Add number slider option

--- ch.io/public/angular/controllers/home.js
+++ ch.io/public/angular/controllers/home.js
@@ -4,10 +4,18 @@ app.controller('homeController', ['$scope', 'dataService', function ($scope, dat
     dataService.sendRawTextInput($scope.customerInput);
   }
   $scope.data = [{"question": "This is q1",
-                  "options": ["Choice 1", "Choice 2"]
+                  "options": ["Choice 1", "Choice 2"],
+                  "key": "q1",
+                  "type": "number",
+                  "min": "Owl",
+                  "max": "Tiger"
                 },{"question": "This is q2",
-                  "options": ["Choice 3", "Choice 4"]
+                  "options": ["Choice 3", "Choice 4"],
+                  "key": "q2",
+                  "type": "choice"
                 },{"question": "This is q3",
-                  "options": ["Choice 5", "Choice 6"]
+                  "options": ["Choice 5", "Choice 6"],
+                  "key": "q3",
+                  "type": "choice"
                 }];
 }]);
--- ch.io/public/angular/controllers/survey.js
+++ ch.io/public/angular/controllers/survey.js
@@ -1,8 +1,8 @@
 app.controller('surveyController', ['$scope', 'dataService', function ($scope, dataService) {
-	//This is the data sent in from the survey directive
-	$scope.answers = Array();
-	$scope.questionObj = [];
-	for (var i = 0, len = $scope.data.length; i < len; i++) {
+    //This is the data sent in from the survey directive
+    $scope.answers = Array();
+    $scope.questionObj = [];
+    for (var i = 0, len = $scope.data.length; i < len; i++) {
        $scope.questionObj.push($scope.data[i]);
     }
 
@@ -10,31 +10,63 @@ app.controller('surveyController', ['$scope', 'dataService', function ($scope, d
     $scope.isLastQuestion = (($scope.questionObj.length - 1) <= $scope.currentQuestion);
     $scope.isFirstQuestion = true;
 
-	$scope.question = $scope.questionObj[$scope.currentQuestion].question;
-	$scope.options = $scope.questionObj[$scope.currentQuestion].options;
-    
+    $scope.question = $scope.questionObj[$scope.currentQuestion].question;
+    $scope.options = $scope.questionObj[$scope.currentQuestion].options;
+    $scope.key = $scope.questionObj[$scope.currentQuestion].key;
+    $scope.type = $scope.questionObj[$scope.currentQuestion].type;
+    $scope.sliderval = 5;
+    switch ($scope.type) {
+        case "number":
+            $scope.min = $scope.questionObj[$scope.currentQuestion].min;
+            $scope.max = $scope.questionObj[$scope.currentQuestion].max;
+            break;
+
+        case "choice":
+            break;
+
+        case "multiselect":
+            break;
+    }
+
     $scope.nextQuestion = function(ans) {
-    	$scope.currentQuestion += 1;
+        $scope.currentQuestion += 1;
         $scope.isFirstQuestion = ($scope.currentQuestion <= 0);
         $scope.isLastQuestion = (($scope.questionObj.length - 1) < $scope.currentQuestion);
-        if (ans) {
-	        $scope.answers.push(ans);
-        } else {
-        	$scope.answers.push("");
+
+        switch ($scope.type) {
+            case "number":
+                $scope.answers[$scope.key] = $scope.sliderval;
+                break;
+
+            case "choice":
+                if (ans) {
+                    $scope.answers[$scope.key] = ans;
+                } else {
+                   $scope.answers.push("");
+                }
+                break;
+
+            case "multiselect":
+                break;
         }
         if (($scope.questionObj.length - 1) < $scope.currentQuestion) {
- 			//Finish survey.. continue to post processing
- 			$scope.question = "Done!";
- 			$scope.options = [];
- 			console.log($scope.answers);
+                //Finish survey.. continue to post processing
+                $scope.question = "Done!";
+                $scope.options = [];
+                console.log($scope.answers);
         } else {
-    	    $scope.question = $scope.questionObj[$scope.currentQuestion].question;
-        	$scope.options = $scope.questionObj[$scope.currentQuestion].options;
+            $scope.question = $scope.questionObj[$scope.currentQuestion].question;
+            $scope.options = $scope.questionObj[$scope.currentQuestion].options;
+            $scope.key = $scope.questionObj[$scope.currentQuestion].key;
+            $scope.type = $scope.questionObj[$scope.currentQuestion].type;
         }
     }
-/*  --Unused Prev Question 
+    $scope.updateSlider = function(val) {
+        $scope.sliderval = val;
+    }
+    /*  --Unused Prev Question
     $scope.prevQuestion = function() {
-    	$scope.currentQuestion -= 1;
+        $scope.currentQuestion -= 1;
         $scope.isFirstQuestion = ($scope.currentQuestion <= 0);
         $scope.isLastQuestion = (($scope.questionObj.length - 1) <= $scope.currentQuestion);
         console.log($scope.isFirstQuestion);

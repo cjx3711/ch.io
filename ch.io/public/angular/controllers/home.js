69b3584b5c1d3c30aeca77a74b176f94cc0f1f7a
Yao Xiang <yaoxiang@Yaos-MacBook-Pro.local>
2016-09-25 05:16:32 -0700

    Add routing for home page

--- ch.io/public/angular/controllers/home.js
+++ ch.io/public/angular/controllers/home.js
@@ -1,11 +1,22 @@
-app.controller('homeController', ['$scope', '$rootScope', 'dataService', function ($scope, $rootScope, dataService) {
+app.controller('homeController', ['$scope', '$rootScope', 'dataService', '$timeout', '$location', function ($scope, $rootScope, dataService, $timeout, $location) {
   $rootScope.dark = true;
   $scope.message = dataService.welcomeMessage;
   $scope.brainimg = "../assets/brain.png";
   $scope.micimg = "../assets/mic.png";
 
+  $scope.customerInput = "";
   $scope.submitData = function() {
     dataService.sendRawTextInput($scope.customerInput);
+  }
+
+  $scope.updateInput = function(input) {
+    console.log(input);
+    if (input == "i" || input == "I") {
+        $scope.customerInput = "I have 5 hours. What can I do?";
+    }
+    $timeout(function () {
+        $location.url('/itinerary');
+    }, 1000);
   }
 
 

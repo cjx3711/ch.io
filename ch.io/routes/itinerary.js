9c982559903c39e6c39fc0c6d28b7e417d141042
wyrm <mervyn@fellowrobots.com>
2016-09-24 18:49:21 -0700

    Saving as a checkpoint. Need to develop algorithm to choose Actions by looking at preferences and time available.

--- ch.io/routes/itinerary.js
+++ ch.io/routes/itinerary.js
@@ -4,7 +4,7 @@ var router = express.Router();
 // Return format
 // [
 //   {
-//     type: "Event",
+//     type: "Action",
 //     data: object
 //   },
 //   {
@@ -14,9 +14,30 @@ var router = express.Router();
 // ]
 //
 /* GET users listing. */
+
+// Flow:
+// What is your spirit animal? intensity: Grandfather sloth/Bunny on a sugar high
+// Do you like walking? distance: I'd drive to my car if I could/My name is Johnny, I'm a walker
+// Are you hungry? Not right now/I'm hangry
+
 router.get('/generate', function(req, res, next) {
   var hours = req.param('hours');
-  res.send('Hours:' + hours); // RETURN
+  var preferences = req.param('preferences')
+
+  result = []
+  data = {
+    id: 17,
+    name: "Burberry",
+    pos: { x: 5324, y: 2803 },
+    category: "Retail",
+    tags: ["luxury", "fashion", "fragrances", "fragrance", "sunglasses", "shades", "cosmetics", "outerwear", "clothes", "Burberry"],
+    timeTaken: 20
+  }
+  action = { type: "Action", data: data }
+  result.push(action)
+  travel = { type: "Travel", time: 4 }
+  result.push(travel)
+  res.send(result); // RETURN
   // res.send('respond with a resource');
   var hours = req.body.hours;
 

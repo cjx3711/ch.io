31e9e71222b1292a337a006089ea8396e7079784
Yao Xiang <yaoxiang@Yaos-MacBook-Pro.local>
2016-09-25 03:54:11 -0700

    Add stlying for home page

--- ch.io/public/angular/controllers/home.js
+++ ch.io/public/angular/controllers/home.js
@@ -1,6 +1,9 @@
 app.controller('homeController', ['$scope', '$rootScope', 'dataService', function ($scope, $rootScope, dataService) {
   $rootScope.dark = true;
   $scope.message = dataService.welcomeMessage;
+  $scope.brainimg = "../assets/brain.png";
+  $scope.micimg = "../assets/mic.png";
+
   $scope.submitData = function() {
     dataService.sendRawTextInput($scope.customerInput);
   }
